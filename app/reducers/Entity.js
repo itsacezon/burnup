6fedfc4cf3fe70c35d9aa88a1b79e9c6c203f725
Acezon Cay <hello@acezoncay.me>
2017-12-08 10:26:52 +1100

    Added a reducer example

---
+++ app/reducers/Entity.js
@@ -0,0 +1,47 @@
+import {
+  FETCH_ENTITY_REQUEST,
+  FETCH_ENTITY_SUCCESS,
+  FETCH_ENTITY_FAILURE,
+} from 'constants/ActionTypes';
+
+const initialState = {
+  data: undefined,
+  errors: [],
+  isFetching: false,
+};
+
+const _entity = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_ENTITY_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case FETCH_ENTITY_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.data,
+      };
+    case FETCH_ENTITY_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errors: action.errors,
+      };
+  }
+};
+
+export const entity = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_ENTITY_REQUEST:
+    case FETCH_ENTITY_SUCCESS:
+    case FETCH_ENTITY_FAILURE:
+      return {
+        ...state,
+        ..._entity(state, action),
+      };
+    default:
+      return state;
+  }
+};
---
+++ app/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import { entity } from 'reducers/Entity';
+
+export default combineReducers({
+  entity,
+});
